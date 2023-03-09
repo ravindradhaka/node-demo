b04665e5b5bd78074c8cbe60cbf00fcf4f07df32
RAVINDRA DHAKA <30661809+ravindradhaka@users.noreply.github.com>
2023-03-09 19:56:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "node-demo application." });
+});
+
+require("./routes/turorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
